c6b6b5994b66b531d52d7bc5094217a7d5b651e7
Tariq Hassan <145034954+TariqAlhathloul@users.noreply.github.com>
2024-01-06 19:53:17 +0300

    weather list

--- App.js
+++ App.js
@@ -1,18 +1,132 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+// eslint-disable-next-line max-len
+import {View, StyleSheet, SafeAreaView, FlatList, Text, statusBar} from 'react-native';
 import CurrentWeather from './src/components/currentWeather';
+import UpComingWeather from './src/components/UpComingWeather';
+import {Feather} from '@expo/vector-icons';
+/**
+ * Represents an array of weather data.
+ * @type {Array}
+ */
+const DATA = [
+  {
+    dt_text: '2024-01-03 12:00:00',
+    main: {
+      temp: 296.31,
+      feels_like: 296.07,
+      temp_min: 296.2,
+      temp_max: 296.31,
+    },
+    Weather: [
+      {
+        main: 'Rain',
+      },
+    ],
+  },
+  {
+    dt_text: '2024-01-03 12:00:00',
+    main: {
+      temp: 296.31,
+      feels_like: 296.07,
+      temp_min: 296.2,
+      temp_max: 296.31,
+    },
+    Weather: [
+      {
+        main: 'Snow',
+      },
+    ],
+  },
+];
 
-const App = () => {
+/**
+ * Represents an item in the list.
+ * @param {object} props - props
+ * @return {View} - Item
+ */
+
+const Item = (props) => {
+// eslint-disable-next-line react/prop-types
+  const {dt_text, temp_min, temp_max, condition} = props;
   return (
-    <View style={styles.container}>
-      <CurrentWeather />
+    <View>
+      <Feather name={'sun'} size={'50'} color={'white'}/>
+      <Text styles={styles.date}>{dt_text}</Text>
+      <Text styles={styles.temp}>{temp_min}</Text>
+      <Text styles={styles.temp}>{temp_max}</Text>
     </View>
   );
 };
+/**
+ * Represents the app.
+ * @return {View} - App
+ */
+const EmptyList = () => {
+  return (
+    <View>
+      <Text>No Data</Text>
+    </View>
+  );
+};
+/**
+ * Represents the app.
+ * @return {View} - App
+ */
+const App = () => {
+  // eslint-disable-next-line no-unused-vars
+  const renderItem = () => {
+    return (
+      <Item
+        condition={condition.Weather[0].main}
+        dt_text={dt_text}
+        temp_min={temp_min}
+        temp_max={temp_max}
+      />);
+  };
+
+  return (
+    <SafeAreaView style={styles.SafeContainer}>
+      <Text>Up Coming Weather</Text>
+      <FlatList
+        data={DATA}
+        renderItem={({item}) => <UpComingWeather data={item} />}
+        keyExtractor={(item) => item.dt_text}
+        ListEmptyComponent={EmptyList}
+      />
+    </SafeAreaView>
+  );
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 50,
+    flex: 1,
+    backgroundColor: 'plum',
+    marginTop: statusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: 'plum',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: 'row',
+    alignContent: 'center',
+    borderWidth: 5,
+  },
+  temp: {
+    fontSize: 20,
+    color: 'white',
+  },
+  date: {
+    fontSize: 15,
+    color: 'white',
   },
 });
+
 export default App;
+
+
